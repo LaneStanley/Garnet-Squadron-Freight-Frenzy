a55d555c224d308ddcf6a8e1bb85f1d683ce7ce0
saber1173 <46358399+saber1173@users.noreply.github.com>
2021-11-12 19:38:20 -0500

    PID class

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Utils/PID.java
@@ -0,0 +1,28 @@
+package org.firstinspires.ftc.teamcode.Utils;
+
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.util.ElapsedTime;
+
+public class PID {
+    private double kp = 0;
+    private double ki = 0;
+    private double kd = 0;
+    
+    private double integral = 0;
+    private double previousError = 0;
+    
+    public PID(double p, double i, double d) {
+        kp = p;
+        ki = i;
+        kd = d;
+    }
+    
+    public double output(double error) {
+        double derivative = error - previousError;
+        integral += error;
+        previousError = error;
+        
+        return  error * kp + integral * ki + derivative * kd;
+    }
+    
+}
